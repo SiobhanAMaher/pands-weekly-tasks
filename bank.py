776692649c614d423ea0b7cac3099caf8979cc60
Siobhán Maher <siobhan.adelle.maher@gmail.com>
2025-02-17 21:07:24 +0000

    adding bank.py

---
+++ bank.py
@@ -0,0 +1,10 @@
+#bank.py
+#programme which asks user to enter integer amounts and adds them
+#author: Siobhán Maher
+
+number1=int(input('Enter amount (in cent): ')) # can divide by 100 here either instead of dividng sum by /100
+number2=int(input('Enter amount (in cent): ')) # can divide by 100 here either instead of dividng sum by /100
+print ('The sum of these amounts is €',((number1 + number2)/100))
+
+
+
